4aedf716b2a2fbaa847faacb74d71899c04c28fb
Krishna Prasad <skprasadu@yahoo.com>
2019-02-03 11:55:13 -0800

    Updates

--- precache-manifest.4f362e5cd1907cdaffbec3021035463d.js
+++ precache-manifest.4f362e5cd1907cdaffbec3021035463d.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/kp-react-portfolio/static/media/nasa_hp.e9c4771e.png"
   },
   {
-    "revision": "a7f432f863629aa91643",
+    "revision": "f5d14e588db82d77b0bb",
     "url": "/kp-react-portfolio/static/css/main.850cadd3.chunk.css"
   },
   {
@@ -84,28 +84,36 @@ self.__precacheManifest = [
     "url": "/kp-react-portfolio/static/media/tsa.118aa0e8.png"
   },
   {
-    "revision": "a7f432f863629aa91643",
-    "url": "/kp-react-portfolio/static/js/main.a7f432f8.chunk.js"
+    "revision": "f5d14e588db82d77b0bb",
+    "url": "/kp-react-portfolio/static/js/main.f5d14e58.chunk.js"
   },
   {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/kp-react-portfolio/static/media/icons.0ab54153.woff2"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/kp-react-portfolio/static/media/flags.9c74e172.png"
   },
   {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/kp-react-portfolio/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/kp-react-portfolio/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
     "revision": "faff92145777a3cbaf8e7367b4807987",
     "url": "/kp-react-portfolio/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/kp-react-portfolio/static/media/outline-icons.ef60a4f6.woff"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/kp-react-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/kp-react-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/kp-react-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/kp-react-portfolio/static/media/outline-icons.ad97afd3.ttf"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
@@ -116,24 +124,16 @@ self.__precacheManifest = [
     "url": "/kp-react-portfolio/static/media/outline-icons.82f60bd0.svg"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/kp-react-portfolio/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/kp-react-portfolio/static/media/brand-icons.e8c322de.woff2"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/kp-react-portfolio/static/media/outline-icons.ad97afd3.ttf"
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/kp-react-portfolio/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/kp-react-portfolio/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/kp-react-portfolio/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -144,8 +144,8 @@ self.__precacheManifest = [
     "url": "/kp-react-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/kp-react-portfolio/static/media/flags.9c74e172.png"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/kp-react-portfolio/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "a1a749e89f578a49306ec2b055c073da",
@@ -164,7 +164,7 @@ self.__precacheManifest = [
     "url": "/kp-react-portfolio/static/css/1.4b6cd11b.chunk.css"
   },
   {
-    "revision": "4c95a51fe4441404237e6bb75b0f8e50",
+    "revision": "8420ba790290c4fdcc02c2f3a33f88b7",
     "url": "/kp-react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kp-react-portfolio/precache-manifest.fb1905839abd44800f303705a9e1aef9.js"
+  "/kp-react-portfolio/precache-manifest.4f362e5cd1907cdaffbec3021035463d.js"
 );
 
 workbox.clientsClaim();
